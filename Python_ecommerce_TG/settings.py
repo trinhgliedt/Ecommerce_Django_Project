2d46bbae8f0b375a630c5657ce51148a68e609da
Trinh Gliedt <chuot2008@gmail.com>
2020-11-08 22:53:30 -0800

    test display shopping cart deployment

--- Python_ecommerce_TG/settings.py
+++ Python_ecommerce_TG/settings.py
@@ -24,8 +24,10 @@ SECRET_KEY = 'b_s!l8v2u8j61^(*m9fh%&3(xz$=vewu9w-cs2(*v-d#bo^d0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = ['18.191.91.140']
+# ALLOWED_HOSTS = ['18.191.91.140']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- main/views.py
+++ main/views.py
@@ -205,54 +205,16 @@ def display_success(request):
 
 
 def display_shopping_cart(request):
-    quantity_in_cart = 0
-
-    #cart_detail: more details about this cart. Each item in the cart is stored in an array called item_info
-    cart_detail = []
-    #declare variable for this product, to pull put unit price and product name from model
-    this_product = None
-    total_before_tax = 0
-    total_before_tax_str = ""
-    sales_tax_str =""
-    total_after_tax=""
-    if "cart_dict" not in request.session:
-        request.session["cart_dict"]={}
-        request.session["quantity_in_cart"]=0
-    if request.session["cart_dict"] != {}:
-        for key in request.session["cart_dict"]:
-            #quantity_in_cart: to display on top nav_bar
-            quantity_in_cart += request.session["cart_dict"][key]
-            #item_info: temporary variable inside loop, to store product id, name, unit price and quantity for each item in cart
-            item_info = []
-            this_product = Product.objects.get(id=int(key))
-            item_info.append(key) #product id, item_info[0]
-            item_info.append(this_product.name) # product name, item_info[1]
-            
-            #locale.setlocale is to convert to currency, with decimals and comma for thousand. I needed to make a few new string variables since the original float variables are needed for calculation
-            locale.setlocale(locale.LC_ALL, 'en_US')
-
-            item_info.append(locale.currency(this_product.unit_price, symbol=False, grouping=True)) # unit price, item_info[2]
-            item_info.append(request.session["cart_dict"][key])#quantity in cart for this item, item_info[3]
-            item_info.append(locale.currency(this_product.unit_price*request.session["cart_dict"][key], symbol=False, grouping=True)) #subtotal for this item, item_info[4]
-            total_before_tax += this_product.unit_price*request.session["cart_dict"][key]
-            cart_detail.append(item_info)
-        
-        total_before_tax_str = locale.currency(total_before_tax, symbol=True, grouping=True)
-        sales_tax_rate = 0.095
-        sales_tax = Decimal(total_before_tax)*Decimal(sales_tax_rate)
-        sales_tax_str = locale.currency(sales_tax, symbol=False, grouping=True)
-        total_after_tax = locale.currency((total_before_tax + sales_tax), symbol=True, grouping=True)
+    
 
     context = {
-        "cart_detail" : cart_detail,
-        "quantity_in_cart": quantity_in_cart,
-        "total_before_tax_str": total_before_tax_str,
-        "sales_tax_str": sales_tax_str,
-        "total_after_tax": total_after_tax,
+        "cart_detail" : [],
+        "quantity_in_cart": 0,
+        "total_before_tax_str": 0,
+        "sales_tax_str": 0,
+        "total_after_tax": 0,
     }
-    print('cart_detail:', cart_detail)
     return render(request, "_4_shop_cart.html", context)
-    # return redirect('/')
 
 def process_shopping_cart(request):
     if request.method == "POST":
